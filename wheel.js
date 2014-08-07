df0f20c6c52c5dd27b9706a06f3ea59daba20cc3
Wayne <dev.wayne.ix@gmail.com>
2014-08-07 19:51:11 +0800

    chgwheel

--- wheel.js
+++ wheel.js
@@ -3,16 +3,16 @@
 	$.fn.wheel = function(opts) {
 		return this.each(function() {
 			var $wheel = $(this),
-				$pointer = $(this).children(),
+				$pointer = $(this).next(),
 				options = $.extend(options, opts, $.fn.wheel.defaults);
-			$wheel.css('position', 'relative');
+			$wheel.parent().css('position', 'relative');
 			centershow($pointer);
 		})
 	}
 
 
 	function centershow(divName) {
-		var container = $(divName).parent();
+		var container = $(divName).prev();
 		var top = (container.height() - $(divName).height()) / 2;
 		var left = (container.width() - $(divName).width()) / 2;
 		var scrollTop = container.scrollTop() || 0;
@@ -26,7 +26,8 @@
 	}
 
 
-	$.fn.rotate = function(final_deg) {
+	$.fn.rotate = function(final_deg, callback) {
+		return this.each(function(){		
 		var acc = 1,
 			acc_flag = true;
 		if (final_deg < 180) {
@@ -46,10 +47,14 @@
 
 				if (acc == 1 && deg == final_deg && !acc_flag) {
 					clearInterval(timefunc);
+					if (callback && (callback instanceof Function)) {
+						callback();
+					}
 				}
 			}
 			_this.css('-webkit-transform', 'rotate(' + deg + 'deg)')
 		}, 20);
+		})
 	}
 
 
